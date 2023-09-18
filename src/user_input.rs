16a78c9de4705939eeaa1127ad8800adb216948a
Sigurd Tullander <2935944-sigurdo@users.noreply.gitlab.com>
2023-09-18 12:58:14 +0200

    Detect control and shift key modifiers

--- src/user_input.rs
+++ src/user_input.rs
@@ -1,4 +1,4 @@
-use crossterm::event::{Event, KeyCode};
+use crossterm::event::{Event, KeyCode, KeyModifiers};
 use ratatui::style::Color;
 use std::sync::mpsc::{self};
 
@@ -11,42 +11,53 @@ pub fn handle_user_input(
     redraw_tx: &mpsc::Sender<()>,
 ) -> ResultCustom<()> {
     match event {
-        Event::Key(e) => match e.code {
-            KeyCode::Char('q') => {
-                exit_tx.send(())?;
-            }
-            KeyCode::Char(character) => {
-                match character {
-                    'h' => {
-                        program_state.cursor_position.1 -= 1;
-                    }
-                    'j' => {
-                        program_state.cursor_position.0 += 1;
-                    }
-                    'k' => {
-                        program_state.cursor_position.0 -= 1;
-                    }
-                    'l' => {
-                        program_state.cursor_position.1 += 1;
-                    }
-                    _ => {
-                        program_state.canvas.set_character(
-                            (
-                                program_state.cursor_position.0 as u64,
-                                program_state.cursor_position.1 as u64,
-                            ),
-                            character,
-                        );
-                    }
+        Event::Key(e) => {
+            match e.code {
+                KeyCode::Char('q') => {
+                    exit_tx.send(())?;
                 }
-                redraw_tx.send(())?;
+                KeyCode::Char(character) => {
+                    match character {
+                        'h' => {
+                            program_state.cursor_position.1 -= 1;
+                        }
+                        'j' => {
+                            program_state.cursor_position.0 += 1;
+                        }
+                        'k' => {
+                            program_state.cursor_position.0 -= 1;
+                        }
+                        'l' => {
+                            program_state.cursor_position.1 += 1;
+                        }
+                        _ => {
+                            program_state.canvas.set_character(
+                                (
+                                    program_state.cursor_position.0 as u64,
+                                    program_state.cursor_position.1 as u64,
+                                ),
+                                character,
+                            );
+                        }
+                    }
+                    redraw_tx.send(())?;
+                }
+                _ => {
+                    program_state.a += 1;
+                    redraw_tx.send(())?;
+                }
             }
-            _ => {
-                program_state.a += 1;
-                redraw_tx.send(())?;
+            if e.modifiers.contains(KeyModifiers::CONTROL) {
+                program_state.a += 100;
             }
-        },
-        _ => {}
+            if e.modifiers.contains(KeyModifiers::SHIFT) {
+                program_state.a += 1000;
+            }
+        }
+        _ => {
+            program_state.a += 10;
+            redraw_tx.send(())?;
+        }
     };
     program_state
         .canvas
